a59089600758cab0e5533d4975c82b14b0c74246
Jerry <32352589+Flyingblu@users.noreply.github.com>
2018-12-22 21:08:45 +0800

    Edited get_qs function
    The function now can fetch the answer from AskA and store it as markdown, but it still have some format problems. The `Question` class now has `ans` attribute.

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import re
+from markdownify import markdownify as md
 
 
 class Question:
     def __init__(self, q, date):
         self.body = q
         self.date = date
+        self.ans = None
 
     def __repr__(self):
-        return self.body + ' ' + self.date
+        return self.body + ' ' + self.date + '\n' + self.ans
 
 
 def get_qs():
@@ -30,11 +32,27 @@ def get_qs():
                 if q_body[-1] == '\n':
                     q_body = q_body[:-2]
                 q_list.append(Question(q_body, re.findall("\d{4}-\d{2}-\d{2}", piece.get_text())[0]))
+            else:
+                tmp_q = q_list.pop()
+                tmp_q.ans = re.sub(pattern="\(\/AskA\/", string=md(piece), repl="(https://app.xmu.edu.my/AskA/",
+                                   flags=re.IGNORECASE)
+                q_list.append(tmp_q)
 
         cnt += 1
+    return q_list
 
-    pprint(q_list)
+
+def to_markdown(q_list):
+    file = open("out.md", 'w')
+    for q in q_list:
+        file.write('# ' + q.body + ' ' + q.date + '\n')
+        file.write(q.ans)
+
+    file.close()
+
 
 
 if __name__ == "__main__":
-    get_qs()
+    to_markdown(get_qs())
+
+
