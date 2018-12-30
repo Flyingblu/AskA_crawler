4ce63683dc2663e444f3788b09c298709e69399e
Jerry <realflyingblu@gmail.com>
2018-12-30 20:47:02 +0800

    Add a bunch of things

--- app/acrawler.py
+++ app/acrawler.py
@@ -15,34 +15,33 @@ class Question:
         return '# ' + self.title + ' ' + self.date + '\n'
 
 
-def get_qs():
+def get_qs(page):
     q_list = []
-    cnt = 1
-    while True:
-        result = requests.get("https://app.xmu.edu.my/AskA/?p={0}&CategoryID=0".format(cnt))
-        soup = BeautifulSoup(result.text, 'lxml')
-        soup = soup.find_all(class_="table table-striped")
-        tr_list = soup[0].find_all("tr")
-        if not tr_list:
-            break
+    result = requests.get("https://app.xmu.edu.my/AskA/?p={0}&CategoryID=0".format(page))
+    soup = BeautifulSoup(result.text, 'lxml')
+    soup = soup.find_all(class_="table table-striped")
+    tr_list = soup[0].find_all("tr")
+    if not tr_list:
+        return json.dumps([])
 
-        for piece in tr_list:
-            if piece.find_all(id="showDetail"):
-                q_body = piece.strong.get_text()
-                if q_body[-1] == '\n':
-                    q_body = q_body[:-2]
-                q_list.append(Question(q_body, datetime.strptime(re.findall("\d{4}-\d{2}-\d{2}", piece.get_text())[0], "%Y-%m-%d")))
-            else:
-                answer = piece.find_all("div")
-                ans_str = ""
-                for div in answer:
-                    ans_str += str(div)
-                tmp_q = q_list.pop()
-                tmp_q.body = re.sub(pattern="\(\/AskA\/", string=ans_str, repl="(https://app.xmu.edu.my/AskA/",
-                                    flags=re.IGNORECASE).strip()
-                q_list.append(tmp_q)
+    for piece in tr_list:
+        if piece.find_all(id="showDetail"):
+            q_body = piece.strong.get_text()
+            if q_body[-1] == '\n':
+                q_body = q_body[:-2]
+            q_list.append(
+                Question(q_body, re.findall("\d{4}-\d{2}-\d{2}", piece.get_text())[0] + 'T'))
+        else:
+            answer = piece.find_all("div")
+            ans_str = ""
+            for div in answer:
+                ans_str += str(div)
+            tmp_q = q_list.pop()
+            tmp_q.body = re.sub(pattern='src="\/AskA\/', string=ans_str, repl='src="https://app.xmu.edu.my/AskA/',
+                                flags=re.IGNORECASE).strip()
+            print(tmp_q.body)
+            q_list.append(tmp_q)
 
-        cnt += 1
     return json.dumps(q_list, default=lambda o: o.__dict__, indent=4)
 
 
@@ -63,4 +62,4 @@ def to_json_file(*json_obj):
 
 
 if __name__ == "__main__":
-    to_json_file(*get_qs())
+    to_json_file(*get_qs(1))
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from app import flask_app
 from app import acrawler
+from flask import request
 
 
-@flask_app.route('/')
-@flask_app.route('/index')
+@flask_app.route('/v2/linc/')
 def index():
-    return acrawler.get_qs()
+    return acrawler.get_qs(request.args.get('p'))
